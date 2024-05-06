f4592c9fc933d66d95b18bdebeffa60d6fe27fee
Biswajit Ganguly <biswajit29nov@gmail.com>
2024-05-06 18:11:11 +0800

    Update profile.js

--- profile.js
+++ profile.js
@@ -43,7 +43,7 @@ function renderGridItems(itemsData) {
             <img src="https://via.placeholder.com/400x200" alt="Card Image" class="w-full h-48 object-cover">
             <div class="p-4">
                 <h2 class="text-xl font-semibold text-gray-800 mb-2">${item.title}</h2>
-                <p class="text-gray-600">${repo.description || 'No description'}</p>
+                <p class="text-gray-600">${item.description || 'No description'}</p>
             </div>
         `;
 
